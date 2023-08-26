d53a82b0f5b2a2c2c7a167329bfde4726dd32625
sergio-sak <sakellagi@gmail.com>
2023-08-26 14:13:30 +0300

    File input almost done

--- freqanalyze.cpp
+++ freqanalyze.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 #include <unordered_map>
 #include <cstdio>
+#include <algorithm>
+
+bool comp(std::pair<std::string, long> p1, std::pair<std::string, long> p2){
+    return p1.second > p2.second;
+}
 
 int main(){
     std::string input_mode; //File or terminal
@@ -11,8 +16,13 @@ int main(){
     }
     while (input_mode != "file" && input_mode != "terminal");
     if(input_mode == "file"){
-
+        std::unordered_map<std::string, long> text;
+        std::string word;
+        while(scanf("%s", &word) != EOF){
+            scanf("%s", &word);
+            text[word]++;
+        }
     }
-    else {
-    }
+    std::vector<std::pair<std::string, long>> sorted(text.begin(), text.end());
+    std::sort(sorted.begin(), sorted.end(), comp);
 }
