fc3432ed16f2677393f912e38bc008853ab10133
sergio-sak <sakellagi@gmail.com>
2023-08-26 16:50:28 +0300

    Completely finishd the file input modified testfle

--- freqanalyze.cpp
+++ freqanalyze.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <cstdio>
 #include <algorithm>
+#include <fstream>
+#include <cctype>
 
 bool comp(std::pair<std::string, long> p1, std::pair<std::string, long> p2){
     return p1.second > p2.second;
@@ -13,20 +14,26 @@ int main(){
     do {
         std::cout<<"Input through terminal or files(terminal/file): ";
         std::cin>>input_mode;
-    }
+    }               
     while (input_mode != "file" && input_mode != "terminal");
-    std::unordered_map<std::string, long> text_freq;
-    std::string word;
+
+    std::unordered_map<std::string, long> freq;
+    std::string word;   
+    std::ifstream infile;
+    infile.open("file.in");
     if(input_mode == "file"){
-        freopen("file.in", "r", stdin);
-        while(scanf("%s", &word) != EOF){
-            scanf("%s", &word);
-            text_freq[word]++;
+        while(infile>>word){
+            if(std::ispunct(word.back())){
+                word.pop_back();
+                freq[word]++;
+                continue;        
+            }
+            freq[word]++;        
         }
     }
-    std::vector<std::pair<std::string, long>> sorted(text_freq.begin(), text_freq.end());
+    std::vector<std::pair<std::string, long>> sorted(freq.begin(), freq.end());
     std::sort(sorted.begin(), sorted.end(), comp);
     for(const auto& element:sorted){
-        std::cout<<"Word: "<<element.first<<" Amount of times found: "<<element.second<<std::endl;
+        std::cout<<"Word: \""<<element.first<<"\". Amount of times found: "<<element.second<<std::endl;
     }
 }
